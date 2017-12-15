701e6dab76aed742132ea2df91405c018cbf898e
Mohammed Nokri <nokri.mohammed@gmail.com>
2017-12-15 10:12:46 +0000

    Added Usage Guid

--- README.md
+++ README.md
@@ -17,5 +17,36 @@ A lean responsive Flexbox Carousel/Slider with smooth transitions and text pagin
 
 <br />
 
+# Usage
+```
+    <div class="wrapper wrapper-flex-slider">
+        <div class="container">
+            <div class="child" id="child1"> <!-- Slide 1 --> </div>
+            <div class="child" id="child2"> <!-- Slide 1 --> </div>
+            <div class="child" id="child3"> <!-- Slide 1 --> </div>
+            <div class="child" id="child4"> <!-- Slide 1 --> </div>
+        </div>
+
+        <div id="pagination-wrap">
+            <ul></ul>
+        </div>
+
+        <!--controls-->
+        <div class="btns" id="next"><i class="fa fa-chevron-right"></i></div>
+        <div class="btns" id="previous"><i class="fa fa-chevron-left"></i></div>
+    </div>
+ </addr>
+ 
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha256-k2WSCIexGzOj3Euiig+TlR8gA0EmPjuc79OEeY5L45g=" crossorigin="anonymous"></script>
+  <script src="js/Jquery.FlexSlider.js"></script>
+  <script>
+        $(document).ready(function () {
+            $(".wrapper-flex-slider").flexSlider();
+        });
+  </script>
+```
+<br />
+
 # Demo
 <a href="https://codepen.io/NokriMohammed/pen/POMoBR">codepen.io/NokriMohammed</a>
+
--- js/Jquery.FlexSlider.js
+++ js/Jquery.FlexSlider.js
@@ -1,34 +1,37 @@
-﻿(function ( $ ) {
- 
+/**
+ * @author Mohammed Nokri <nokri.mohammed@gmail.com>
+ */
+(function ( $ ) {
+
     $.fn.flexSlider = function( options ) {
- 
+
         // This is the easiest way to have default options.
         var settings = $.extend({
             index: 0,
             width: $( window ).width()
         }, options );
-      
+
          var nSlides = $(this).find(".child").length;
          var widthSlider = settings.width * nSlides;
          var _container = $(this).find(".container");
          var _next =  $(this).find("#next");
-         var _previous =  $(this).find("#previous"); 
-      
+         var _previous =  $(this).find("#previous");
+
         $(_container).css({width: widthSlider});
-      
+
         //Create a pagination
         for(i=0; i<nSlides; i++){
            var li = document.createElement('li');
            $(li).data("index",i);
-           $(this).find("#pagination-wrap ul").append(li);	  
+           $(this).find("#pagination-wrap ul").append(li);
         }
-        
+
         $('#pagination-wrap ul li').click( function(){
            var index = $(this).data("index");
            if(index >= 0 && index <= nSlides)  slideTo(index);
         });
         pagination() ;
-        
+
        //automatic slider
         var autoSlider = setInterval(function(){slideTo("Right")}, 3000);
 
@@ -39,7 +42,7 @@
            case "Right":  settings.index = ++settings.index==nSlides ?  0 : settings.index++; break;
            default: settings.index = (direction >= 0 && direction <= nSlides) ? direction : 0;  break;
          }
-          $(_container).css('-webkit-transform', 'translateX('+ (-(settings.width*settings.index)) +'px)'); 
+          $(_container).css('-webkit-transform', 'translateX('+ (-(settings.width*settings.index)) +'px)');
           pagination () ;
         }
 
@@ -48,7 +51,7 @@
           $('#pagination-wrap ul li:eq('+settings.index+')').addClass('active');
         }
 
-       //next slide 	
+       //next slide
         $(_next).click(function(){ slideTo("Right"); });
 
         //previous slide
@@ -56,13 +59,11 @@
 
         //hide/show controls/btns when hover pause automatic slide when hover
         $(this).hover(
-          function(){ $(this).addClass('active'); clearInterval(autoSlider); }, 
+          function(){ $(this).addClass('active'); clearInterval(autoSlider); },
           function(){ $(this).removeClass('active'); autoSlider = setInterval(function(){slideTo("Right")}, 3000); }
         );
 
         return this;
     };
- 
+
 }( jQuery ));
-
-
